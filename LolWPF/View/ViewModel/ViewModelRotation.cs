af39ed1d5427053990937c54fbbea375e43248ee
Saitama267 <85219557+Saitama267@users.noreply.github.com>
2023-05-08 21:31:36 +0200

    expand rotation

--- LolWPF/View/ViewModel/ViewModelRotation.cs
+++ LolWPF/View/ViewModel/ViewModelRotation.cs
@@ -18,19 +18,39 @@ namespace LolWPF.View.ViewModel
         public string Champ8 { get; set; }
         public string Champ9 { get; set; }
         public string Champ10 { get; set; }
+        public string Champ11 { get; set; }
+        public string Champ12 { get; set; }
+        public string Champ13 { get; set; }
+        public string Champ14 { get; set; }
+        public string Champ15 { get; set; }
+        public string Champ16 { get; set; }
+        public string Champ17 { get; set; }
+        public string Champ18 { get; set; }
+        public string Champ19 { get; set; }
+        public string Champ20 { get; set; }
 
-        public ViewModelRotation( string name1, string name2, string name3, string name4, string name5, string name6, string name7, string name8, string name9, string name10)
+        public ViewModelRotation( List<string> freeRotation)
         {
-            Champ1 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + name1 + "_0.jpg";
-            Champ2 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + name2 + "_0.jpg";
-            Champ3 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + name3 + "_0.jpg";
-            Champ4 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + name4 + "_0.jpg";
-            Champ5 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + name5 + "_0.jpg";
-            Champ6 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + name6 + "_0.jpg";
-            Champ7 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + name7 + "_0.jpg";
-            Champ8 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + name8 + "_0.jpg";
-            Champ9 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + name9 + "_0.jpg";
-            Champ10 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + name10 + "_0.jpg";
+            Champ1 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + freeRotation[0] + "_0.jpg";
+            Champ2 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + freeRotation[1] + "_0.jpg";
+            Champ3 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + freeRotation[2] + "_0.jpg";
+            Champ4 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + freeRotation[3] + "_0.jpg";
+            Champ5 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + freeRotation[4] + "_0.jpg";
+            Champ6 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + freeRotation[5] + "_0.jpg";
+            Champ7 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + freeRotation[6] + "_0.jpg";
+            Champ8 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + freeRotation[7] + "_0.jpg";
+            Champ9 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + freeRotation[8] + "_0.jpg";
+            Champ10 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + freeRotation[9] + "_0.jpg";
+            Champ11 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + freeRotation[10] + "_0.jpg";
+            Champ12 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + freeRotation[11] + "_0.jpg";
+            Champ13 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + freeRotation[12] + "_0.jpg";
+            Champ14 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + freeRotation[13] + "_0.jpg";
+            Champ15 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + freeRotation[14] + "_0.jpg";
+            Champ16 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + freeRotation[15] + "_0.jpg";
+            Champ17 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + freeRotation[16] + "_0.jpg";
+            Champ18 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + freeRotation[17] + "_0.jpg";
+            Champ19 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + freeRotation[18] + "_0.jpg";
+            Champ20 = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/" + freeRotation[19] + "_0.jpg";
         }
 
     }
